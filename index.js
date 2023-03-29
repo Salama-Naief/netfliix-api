f306f0d9d3ddaff6290c671b4c5e0300a07255c8
SalamaNaief <eng.salama.naief@gmail.com>
2023-03-29 08:02:42 +0200

    port

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const express =require("express");
-const mongoose =require("mongoose");
-const dotenv =require("dotenv");
-const cors =require("cors");
-const app =express();
-const auth=require("./routes/auth");
-const user=require("./routes/user");
-const movie=require("./routes/movie");
-const list=require("./routes/list");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const app = express();
+const auth = require("./routes/auth");
+const user = require("./routes/user");
+const movie = require("./routes/movie");
+const list = require("./routes/list");
 
+const port = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
 app.use(cors());
-mongoose.connect(process.env.MONGODB_URL).then(()=>console.log("database connected succeffuly")).catch((e)=>console.log("database error",e));
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("database connected succeffuly"))
+  .catch((e) => console.log("database error", e));
 
-app.use("/api/auth",auth);
-app.use("/api/users",user);
-app.use("/api/movies",movie);
-app.use("/api/lists",list);
+app.use("/api/auth", auth);
+app.use("/api/users", user);
+app.use("/api/movies", movie);
+app.use("/api/lists", list);
 
-app.listen(5000,()=>{
-    console.log("server is running on port 5000")
-})
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
